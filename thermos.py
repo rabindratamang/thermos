93f3496b67d893d765c043e0918859606ca16070
rabindratamang <rabindra.tamang@yarshastudio.com>
2019-07-04 21:41:11 +0545

    add form post

--- thermos.py
+++ thermos.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
+from logging import DEBUG
+
 
 app = Flask(__name__)
+app.logger.setLevel(DEBUG)
 
 
 class User:
@@ -12,14 +16,31 @@ class User:
         return f"{self.first} {self.last}"
 
 
+bookmarks = []
+
+
+def store_bookmark(url):
+    bookmarks.append(dict(
+        url=url,
+        user='rabindra tamang',
+        date=datetime.utcnow()
+    ))
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title="Title", user=User("Rabindra", "Tamang"))
 
 
-@app.route('/add-bookmark')
+@app.route('/add-bookmark', methods=['GET','POST'])
 def add_bookmark():
+    if request.method == 'POST':
+        url = request.form['url']
+        store_bookmark(url)
+        app.logger.debug('stored_url : '+url)
+        return redirect(url_for('index'))
+
     return render_template('add.html')
 
 
