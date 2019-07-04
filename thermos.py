be2ddb08571a5395c4259b2058e409db888ca6f0
rabindratamang <rabindra.tamang@yarshastudio.com>
2019-07-04 21:11:57 +0545

    update basic view and template

--- thermos.py
+++ thermos.py
@@ -18,5 +18,20 @@ def index():
     return render_template('index.html', title="Title", user=User("Rabindra", "Tamang"))
 
 
+@app.route('/add-bookmark')
+def add_bookmark():
+    return render_template('add.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == "__main__":
     app.run(debug=True)
