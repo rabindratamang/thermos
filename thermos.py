390d658df0d619b0733a8f07745dd7409f587db2
rabindratamang <rabindra.tamang@yarshastudio.com>
2019-07-05 15:03:08 +0545

    minor fixes and add flask-wtf

--- thermos.py
+++ thermos.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from datetime import datetime
-from logging import DEBUG
-
 
 app = Flask(__name__)
-app.logger.setLevel(DEBUG)
+app.config['SECRET_KEY'] = b'\xb9\xa0(6hq\xd0]\x89>C\x8dn\xabo\xff\xf2~&\x1a\x9b\xa2\xef\x84'
 
 
 class User:
@@ -27,18 +25,22 @@ def store_bookmark(url):
     ))
 
 
+def new_bookmarks(num):
+    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title="Title", user=User("Rabindra", "Tamang"))
+    return render_template('index.html', new_bookmarks=new_bookmarks(5))
 
 
-@app.route('/add-bookmark', methods=['GET','POST'])
+@app.route('/add-bookmark', methods=['GET', 'POST'])
 def add_bookmark():
     if request.method == 'POST':
         url = request.form['url']
         store_bookmark(url)
-        app.logger.debug('stored_url : '+url)
+        flash(f"stored bookmark: {url}")
         return redirect(url_for('index'))
 
     return render_template('add.html')
